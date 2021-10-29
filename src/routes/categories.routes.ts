42a21238acc38b711b804ea638584e867741e3de
Delano Filho <delanoalmeidafilho@gmail.com>
2021-10-29 02:07:33 -0300

    chapter II - S.O.L.I.D

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRouter = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -12,12 +13,10 @@ categoriesRouter.get("/", (resquest, response) => {
 categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category Already Exists!" });
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoriesService = new CreateCategoryService(
+    categoriesRepository
+  );
+  createCategoriesService.execute({ name, description });
 
   return response.status(201).send();
 });
