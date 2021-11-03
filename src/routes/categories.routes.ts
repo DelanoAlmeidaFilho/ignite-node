c74cc962b53dbd47a7e8fdf072878b6b21e8ea1b
Delano Filho <delanoalmeidafilho@gmail.com>
2021-11-02 22:58:46 -0300

    chapter II - Continuação da aplicação

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,16 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
 
-const categoriesRouter = Router();
-const categoriesRepository = new CategoriesRepository();
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategory";
 
-categoriesRouter.get("/", (resquest, response) => {
-  const all = categoriesRepository.list();
-  return response.json(all);
+const categoriesRoutes = Router();
+
+categoriesRoutes.get("/", (request, response) => {
+  return listCategoriesController.handle(request, response);
 });
 
-categoriesRouter.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoriesService = new CreateCategoryService(
-    categoriesRepository
-  );
-  createCategoriesService.execute({ name, description });
-
-  return response.status(201).send();
+categoriesRoutes.post("/", (request, response) => {
+  return createCategoryController.handle(request, response);
 });
 
-export { categoriesRouter };
+export { categoriesRoutes };
